96ade5e9b65a8306ec7bf6160ba1b74f2da3d063
sebargith <131008747+sebargith@users.noreply.github.com>
2023-06-30 17:17:52 -0400

    Se agrega enum con Destinos
    Se actualiza el panelinformacion para incluir detalles de recorridos

---
+++ src/main/java/org/example/Destino.java
@@ -0,0 +1,22 @@
+package org.example;
+
+enum Destino {
+    desde_Conce("Cocepcion"),
+    desde_PV("Puerto Varas"),
+    desde_Valdi("Valdivia"),
+    desde_sorno("Osorno"),
+    hacia_Conce("Concepcion"),
+    hacia_PV("Puerto Varas"),
+    hacia_Valdi("Valdivia"),
+    hacia_Osorno("Osorno");
+
+    private final String text;
+
+    Destino(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
--- src/main/java/org/example/PanelInformacion.java
+++ src/main/java/org/example/PanelInformacion.java
@@ -6,15 +6,53 @@ import javax.swing.*;
 
 class PanelInformacion extends JPanel {
 
+    private int column1X = 200; // posicion X de columna "desde"
+    private int column2X = 520; // posicion X de columna "A"
+    private int startY = 180; // posicion Y de ambas columnas
+
+    public PanelInformacion() {
+        setLayout(null);
+
+        ButtonGroup buttonGroup1 = new ButtonGroup();
+        ButtonGroup buttonGroup2 = new ButtonGroup();
+
+        Destino[] Destinos = Destino.values();
+        for (int i = 0; i < Destinos.length; i++) {
+            int columnX = (i < 4) ? column1X : column2X;
+            int rowY = startY + (i % 4) * 30;
+
+            JRadioButton radioButton = new JRadioButton(Destinos[i].getText());
+            radioButton.setBounds(columnX, rowY, 100, 20);
+            radioButton.setFocusPainted(false);
+            radioButton.setBorderPainted(false);
+            radioButton.setOpaque(false);
+
+            if (i < 4) {
+                buttonGroup1.add(radioButton);
+            } else {
+                buttonGroup2.add(radioButton);
+            }
+
+            add(radioButton);
+        }
+    }
+
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(1200, 600); // set this to desired width and height
     }
 
     @Override
-    protected void paintComponent(Graphics g){
+    protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.lightGray);
+        g.fillRect(100, 50, 1000, 500);
+
         g.setColor(Color.black);
-        g.drawRect(0,0,1000,500); // set this to the same size as above
+        g.drawString("Detalles de Compra", 130, 100);
+        g.setColor(Color.black);
+        g.drawString("Desde:", 170, 150);
+        g.setColor(Color.black);
+        g.drawString("A:", 490, 150);
     }
 }
